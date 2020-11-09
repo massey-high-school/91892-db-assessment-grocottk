f2144ea5b1a37491a33796c4ca3be5f3a0a8cdf6
Kahlil <51727305+grocottk@users.noreply.github.com>
2020-11-09 13:18:17 +1300

    Set up of Database
    This is the initial setup of the Database Assessment.

---
+++ 91892_Assessment_Site/bottombit.php
@@ -0,0 +1,19 @@
+        <div class="box side">
+           
+           <h2>Search Area</h2>
+           
+           <p>This is where the search area goes
+            
+        </div> <!-- / side bar -->
+        
+        <div class="box footer">
+            
+            CC Kahlil Grocott 2020
+            
+        </div> <!-- / footer -->
+                
+        
+    </div> <!-- / wrapper -->
+    
+            
+</body>
---
+++ 91892_Assessment_Site/topbit.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+    
+<!-- Database Connect from practice database -->
+    
+<!-- <?php
+    
+    // session_start(); // This allows 'variable transfer between pages'...
+    // include("config.php");
+    
+    // Connects to database...
+    // $dbconnect=mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
+    
+    // if(mysqli_connect_errno()) {
+    // echo "Connection failed:".mysqli_connect_error();
+    // exit;
+    // }
+    
+    ?> -->
+    
+<!-- End of Database Connect -->
+    
+<head>
+    
+    <meta charset="utf-8">
+    <meta name="description" content="games, apps, ">
+    <meta name="GTT" content="Game / App Database">
+    <meta name="keywords" content="games, apps, ratings">
+    
+    <title>Olympics Database</title>
+
+    <!-- for multiple fonts change | to %7c * no spaces*  -->
+    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet">  
+
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/data_style.css">    <!-- custom style sheet -->
+
+</head>
+
+<body>
+    
+    <p class="message">Eek!  Your browser does not support grid.  Please upgrade your system.</p>
+        
+    <div class="wrapper">
+    
+        <!-- logo / small image goes here -->
+        <div class="box logo">
+            <a href="index.php"><img src="images/logo.png" width="331" height="152" alt="Olympic Rings" /></a>
+        </div> <!-- / logo -->
+        
+        <div class="box banner">
+            <h1>Olympics Database</h1>        
+        </div> <!-- / banner -->
+        
